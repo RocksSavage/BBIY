9a13a9442b806e947350d27c2c5ed4f6ebd45786
Brad <bradleylogansherman@gmail.com>
2022-03-29 18:41:50 -0600

    Added printer and font content

---
+++ BigBlueIsYou/TextRenderer/Printer.cs
@@ -0,0 +1,20 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace CS5410
+{
+    public class Printer 
+    {
+        public void printWithOutline(string message, GraphicsDeviceManager m_graphics, SpriteBatch m_spriteBatch, Vector2 stringSize, SpriteFont m_font, Color color, Color outlineColor)
+        {
+            m_spriteBatch.DrawString(m_font, message, new Vector2(stringSize.X-1, stringSize.Y), outlineColor);
+            m_spriteBatch.DrawString(m_font, message, new Vector2(stringSize.X+1, stringSize.Y), outlineColor);
+            m_spriteBatch.DrawString(m_font, message, new Vector2(stringSize.X, stringSize.Y-1), outlineColor);
+            m_spriteBatch.DrawString(m_font, message, new Vector2(stringSize.X, stringSize.Y+1), outlineColor);
+            m_spriteBatch.DrawString(m_font, message, new Vector2(stringSize.X, stringSize.Y), color);
+        }
+    }
+}
